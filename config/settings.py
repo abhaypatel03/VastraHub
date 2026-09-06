cfe7233d35fa240bbc11a37632b6d73703d870d4
roshnipatel0302 <roshnipatelrp2002@gmail.com>
2026-09-06 11:21:53 +0530

    Fix Render allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ SECRET_KEY = 'django-insecure-8v34dg$)**x=4r@l)4r(r5+%%9i-&#z!^^fr1-51&dcl8p$l&$
 
 DEBUG = True
 ALLOWED_HOSTS = [
-    'vastrahub-5.onrender.com',
-    'localhost',
-    '127.0.0.1',
+    "vastrahub-6.onrender.com",
+    "localhost",
+    "127.0.0.1",
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://vastrahub-5.onrender.com'
