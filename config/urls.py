67b99618deb1bec74706e5b8c4d0ab66ce70f19b
roshnipatel0302 <roshnipatelrp2002@gmail.com>
2026-09-06 14:04:11 +0530

    Fix product media image serving

--- config/urls.py
+++ config/urls.py
@@ -17,15 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("products.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
 ]
-
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
