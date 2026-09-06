55188700493c04974744d622589d3fe756d22997
roshnipatel0302 <roshnipatelrp2002@gmail.com>
2026-09-06 11:50:00 +0530

    Fix Render domain

--- config/settings.py
+++ config/settings.py
@@ -22,14 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8v34dg$)**x=4r@l)4r(r5+%%9i-&#z!^^fr1-51&dcl8p$l&$'
 
 DEBUG = False
+
 ALLOWED_HOSTS = [
-    "vastrahub-6.onrender.com",
+    "vastrahub-8.onrender.com",
     "localhost",
     "127.0.0.1",
 ]
+
 CSRF_TRUSTED_ORIGINS = [
-    "https://vastrahub-6.onrender.com",
+    "https://vastrahub-8.onrender.com",
 ]
+
 # =====================================================
 # APPLICATIONS
 # =====================================================
